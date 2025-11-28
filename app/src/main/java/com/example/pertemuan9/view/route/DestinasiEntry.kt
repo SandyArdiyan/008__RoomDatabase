f0572c75f62f9ba6b7db8008d8c2fda772de973e
SandyArdiyan <sanardiyan881@gmail.com>
2025-11-28 22:09:29 +0700

    Menambahkan DestinasiEntry.kt

---
+++ app/src/main/java/com/example/pertemuan9/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan9.view.route
+
+import com.example.pertemuan9.R
+import com.example.pertemuan9.view.route.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
