c6abe4924cba8a4db97f622c0b9c612f61c06ab9
SandyArdiyan <sanardiyan881@gmail.com>
2025-11-28 22:09:42 +0700

    Menambahkan DatabaseSiswa.kt

---
+++ app/src/main/java/com/example/pertemuan9/room/DatabaseSiswa.kt
@@ -0,0 +1,28 @@
+package com.example.pertemuan9.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase() {
+    abstract fun siswaDao(): SiswaDao
+
+    companion object { // Koreksi: companion, bukan companin
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    DatabaseSiswa::class.java,
+                    "siswa_database"
+                )
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
