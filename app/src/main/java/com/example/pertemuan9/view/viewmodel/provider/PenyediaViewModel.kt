e70a9fb3309f137bcd5103a41b2bd425b050724a
SandyArdiyan <sanardiyan881@gmail.com>
2025-12-05 15:39:47 +0700

    Menambahkan saved state handle nya

--- app/src/main/java/com/example/pertemuan9/view/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan9/view/viewmodel/provider/PenyediaViewModel.kt
@@ -3,10 +3,12 @@ package com.example.pertemuan9.view.viewmodel.provider
 
 import android.app.Application
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.pertemuan9.repositori.AplikasiSiswa
+import com.example.pertemuan9.view.viewmodel.DetailViewModel
 import com.example.pertemuan9.view.viewmodel.EntryViewModel
 import com.example.pertemuan9.view.viewmodel.HomeViewModel
 
@@ -14,6 +16,11 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            DetailViewModel(savedStateHandle = this.createSavedStateHandle(),
+                repositoriSiswa = aplikasiSiswa().container.repositoriSiswa )
         }
 
         initializer {
